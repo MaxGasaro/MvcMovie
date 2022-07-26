0c6b654ec6dc283926f202d0089be10d7edfc4af
Massimiliano Gasaro <massimilianogasaro95@gmail.com>
2022-07-26 15:27:21 +0200

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numtimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numtimes;
+
+            return View();
+        }
+    }
+}
